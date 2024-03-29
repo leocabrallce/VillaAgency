536a33b641331203d630f097f3bbce2459033d44
Leonardo Cabral <leocabrallce@gmail.com>
2024-03-29 12:21:15 +0100

    adding express compression

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const compression = require('compression');
 
 // Web routes
 const indexRouter = require('./routes/index');
@@ -14,6 +15,7 @@ const propertyDetailsRouter = require('./routes/property-details');
 const usersRouter = require('./routes/users');
 
 const app = express();
+app.use(compression());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
